e02b43805f112ec6d6f8be1eb832f9d878616a2b
sriley <sarahriley.dev@gmail.com>
2019-04-10 17:01:24 -0400

    component talking

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FriendsList extends Component {
+  render() {
+    return (
+      <div className="friends-list">
+       <h1>This is the friends list</h1>
+      </div>
+    );
+  }
+}
+
+export default FriendsList;
